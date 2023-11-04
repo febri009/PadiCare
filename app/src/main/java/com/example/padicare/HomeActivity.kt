572f555fe031bd094dd5c1b59c93bcfb5423c130
Febriyanti Paramudita <febriyantiparamudita8@gmail.com>
2023-11-04 16:37:23 +0700

    add bottom navigation

--- app/src/main/java/com/example/padicare/HomeActivity.kt
+++ app/src/main/java/com/example/padicare/HomeActivity.kt
@@ -2,10 +2,24 @@ package com.example.padicare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+
+        bottomNavigationView.background = null
+
+        bottomNavigationView.menu.getItem(2).isEnabled = false
+
+        fab.setOnClickListener{
+            Toast.makeText(this, "I'm working dude!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
