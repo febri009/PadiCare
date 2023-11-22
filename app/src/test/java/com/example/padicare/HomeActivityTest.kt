b759c50f14566b4a26d055a6988eb45996195b73
Febriyanti Paramudita <febriyantiparamudita8@gmail.com>
2023-11-22 14:45:47 +0700

    add example unit test

---
+++ app/src/test/java/com/example/padicare/HomeActivityTest.kt
@@ -0,0 +1,15 @@
+package com.example.padicare
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
+
+
+
+
